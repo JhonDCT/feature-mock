280e845d45532fbd662b05a4a0894416fa39db1a
JhonDCT <deyner.catacora@gmail.com>
2026-06-28 22:07:18 -0500

    builder

---
+++ bin/cli.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env bun
+import { startCommand } from '../src/commands/start.command'
+
+await startCommand()
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import { startCommand } from './commands/start.command'
-
-await startCommand()
+export { startCommand } from './commands/start.command'
+export { startMockServer } from './services/mock-server'
+export {
+    getActiveScenario,
+    setActiveScenario,
+    setActiveServer,
+    stopActiveServer,
+} from './services/state-manager'
+export { listsFeatures, listsAcceptanceCriteria, loadResponses } from './utils/fs'
+export type { MockEntry, MockRequest, MockResponse, ResponsesSchema } from './utils/fs'
